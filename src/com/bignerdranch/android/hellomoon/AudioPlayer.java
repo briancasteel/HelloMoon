7934f076582ee276660d5bd17a86468748503044
Brian Casteel <bcasteel@alumni.utexas.net>
2015-08-01 12:15:04 -0500

    Wired up buttons to enable play/stop

--- src/com/bignerdranch/android/hellomoon/AudioPlayer.java
+++ src/com/bignerdranch/android/hellomoon/AudioPlayer.java
@@ -17,7 +17,17 @@ public class AudioPlayer extends java.lang.Object {
     }
 
     public void play(Context c) {
+        stop();
+
         mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+
         mPlayer.start();
     }
 }
--- src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -13,14 +13,34 @@ import android.widget.Button;
 public class HelloMoonFragment extends Fragment{
     private Button mPlayButton;
     private Button mStopButton;
+    private AudioPlayer mPlayer = new AudioPlayer();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 
         mPlayButton = (Button)v.findViewById(R.id.hellomoon_playbutton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.play(getActivity());
+            }
+        });
+
         mStopButton = (Button)v.findViewById(R.id.hellomoon_stopbutton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
     }
 }
